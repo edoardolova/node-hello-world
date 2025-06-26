caca0cbe81c78fc4021f04182e445c2bdcddb6da
Edoardo Lova <edoardolova81@gmail.com>
2025-06-26 16:36:15 +0200

    add axios + axios ajax

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ import chalk from 'chalk';
 console.log("hello world");
 console.log(chalk.red("hello Boolean") );
 console.log(chalk.green.underline.bold("hello there") );
+
+// axios
+import axios from "axios"
+
+axios.get("https://lanciweb.github.io/demo/api/pictures/")
+.then(res => console.log(res.data) )
+.catch(error => console.error(`errore: ${error}`))
